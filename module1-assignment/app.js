3488247e7da3e22667ba5aab3035fdf6c434ee1d
Saikia <tarangita.saikia@dxc.com>
2020-01-31 18:34:54 +0530

    1st assignment solution

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -1,12 +1,26 @@
 (function(){
-	angular.module('myFirstModule', [])
-		.controller('TestController', TestCtrl);
+	angular.module('LunchCheck', [])
+		.controller('LunchCheckController', LunchCheckController);
 		
-	TestCtrl.$inject = ['$scope'];
+	LunchCheckController.$inject = ['$scope'];
 	
-	function TestCtrl($scope){
-		$scope.name = "Tara";
+	function LunchCheckController($scope){
+		
+		$scope.checkLunch = function(lunchDetails){
+			
+			if(lunchDetails === undefined){
+				$scope.lunchMessage = 'Please enter data first';
+			}else{
+				var lunchArr = lunchDetails.split(',');
+				var lunchCount = lunchArr.length;
+				if(lunchCount > 3 ){
+					$scope.lunchMessage = 'Too much!';
+				}else{
+					$scope.lunchMessage = 'Enjoy!';
+				}
+			}
+			
+		};
 	}
-	
 	
 })();
