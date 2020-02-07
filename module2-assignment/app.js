ea96a5a068442c09214b3b726d8dcc165621d40d
Saikia <tarangita.saikia@dxc.com>
2020-02-07 17:59:50 +0530

    2nd assignment solution

---
+++ module2-assignment/app.js
@@ -0,0 +1,46 @@
+(function(){
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', ToBuyController)
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffSvc);
+	
+	ToBuyController.$inject=['ShoppingListCheckOffService'];
+	function ToBuyController(ShoppingListCheckOffService){
+		var toBuyInst = this;
+
+		toBuyInst.items = ShoppingListCheckOffService.getThings();
+		
+		toBuyInst.moveIt = function(Index){
+			ShoppingListCheckOffService.removeItem(Index);
+		};
+	}
+	
+	AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
+	function AlreadyBoughtController(ShoppingListCheckOffService){
+		var alreadyBoughtInst = this;
+		
+		alreadyBoughtInst.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+		
+	}
+	
+	function ShoppingListCheckOffSvc(){
+		var service = this;
+		
+		var boughtThings = [];
+		
+		service.getThings = function(){
+			return things = [{name: "Milk",quantity: 4},{name: "Carrots",quantity: 20},{name: "Chocolates",quantity: 10},{name: "Celery",quantity: 5},{name: "Broccoli", quantity: 5}];
+		};
+		
+		service.removeItem = function(ItemIndex){
+			boughtThings.push(things[ItemIndex]);
+			return things.splice(ItemIndex, 1);
+		};
+		
+		service.getBoughtItems = function(){
+			return boughtThings;
+		}
+	
+	}
+	
+})();
