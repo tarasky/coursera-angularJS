67f884928c2f862ceebd1aa6a5f4bf676aa701ae
Saikia <tarangita.saikia@dxc.com>
2020-02-16 01:56:24 +0530

    3rd assignment - 1

---
+++ module3-assignment/app.js
@@ -0,0 +1,57 @@
+(function(){
+	'use strict';
+	
+	angular.module('NarrowItDownApp', [])
+	.controller('NarrowItDownController', NarrowItDownController)
+	.service('MenuSearchService', MenuSearchService)
+	.directive('foundItems', ListItemsDirective);
+	
+	function ListItemsDirective(){
+		var ddo = {
+			scope : {
+				listSelected : '=getList'
+			},
+			templateUrl : "list.html"
+		};
+		return ddo;
+	}
+	
+	NarrowItDownController.$inject = ['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService){
+		var narrowCtrl = this;
+		
+		//narrowCtrl.isClick = false;
+		narrowCtrl.getMenuItems = function(){
+			MenuSearchService.getMatchedMenuItems(narrowCtrl.searchDesc);
+		};
+		
+		narrowCtrl.found = MenuSearchService.getReqdItems();
+	}
+	
+	MenuSearchService.$inject = ['$http'];
+	function MenuSearchService($http){
+		var menuSvc = this;
+		
+		var foundItems = [];
+		
+		menuSvc.getMatchedMenuItems = function(searchTerm){
+			$http({
+				url : "https://davids-restaurant.herokuapp.com/menu_items.json",
+				method : "GET"
+			})
+			.then(function(response){
+				angular.forEach(response.data.menu_items, function(value, key){
+					if(value.description.indexOf(searchTerm) != -1){
+						foundItems.push(value);
+					}
+				});
+			})
+		}
+		
+		menuSvc.getReqdItems = function(){
+			return foundItems;
+		}
+		
+	}
+	
+})();
