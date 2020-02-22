a63d4934a979e9d0bbcfc2f3a7bf3b8e618fc5da
Saikia <tarangita.saikia@dxc.com>
2020-02-22 22:40:37 +0530

    initial 4th

--- module4-assignment/src/items.controller.js
+++ module4-assignment/src/items.controller.js
@@ -7,6 +7,7 @@ ItemsController.$inject = ['allitems'];
 function ItemsController(allitems){
 	var itemCtrl = this;
 	itemCtrl.item = allitems.menu_items;
+	console.log(allitems.category.short_name);
 	itemCtrl.sel = allitems.category.short_name;
 }
 
