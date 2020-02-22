1928d9850d4791746d8201ae6058288bcd036745
Saikia <tarangita.saikia@dxc.com>
2020-02-22 22:47:59 +0530

    initial 4th

--- module4-assignment/src/items.controller.js
+++ module4-assignment/src/items.controller.js
@@ -6,8 +6,8 @@ angular.module('MenuApp')
 ItemsController.$inject = ['allitems'];
 function ItemsController(allitems){
 	var itemCtrl = this;
+	
 	itemCtrl.item = allitems.menu_items;
-	console.log(allitems.category.short_name);
 	itemCtrl.sel = allitems.category.short_name;
 }
 
